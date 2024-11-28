a911550c110e4e366b8ee35d208f359d8694641a
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-11-28 15:22:25 +0330

    coding db connection

--- src/server.js
+++ src/server.js
@@ -1,13 +1,37 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+//* Load ENV
+
+const productionMode = process.env.NODE_ENV === "Production";
+
+if (!productionMode) {
+  dotenv.config();
+}
 
 function startServer() {
   app.listen(4000, () => {
-    console.log(`Server Running On Port ${process.env.PORT}`);
+    console.log(
+      `Server Running in ${
+        productionMode ? "Production" : "Development"
+      } Mode On Port ${process.env.PORT}`
+    );
   });
+}
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected To DB Successfully : ${mongoose.connection.host}.`);
+  } catch (error) {
+    console.error("Error In DataBase Connection ==>", error), process.exit(1);
+  }
 }
 
 function run() {
   startServer();
+  connectToDB();
 }
 
 run();
