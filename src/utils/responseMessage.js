06dbbcca69b7ded53dc5380ecd8c3c88fb6a2534
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-11-28 22:53:27 +0330

    fix bug

--- src/utils/responseMessage.js
+++ src/utils/responseMessage.js
@@ -1,5 +1,5 @@
 //* Helper function to success response format
-exports.successResponse = (res, statusCode = 200, data) => {
+successResponse = (res, statusCode = 200, data) => {
   return res
     .status(statusCode)
     .json({ status: statusCode, success: true, data });
@@ -7,7 +7,7 @@ exports.successResponse = (res, statusCode = 200, data) => {
 
 //* Helper function to error response format
 
-exports.errorResponse = (res, statusCode, message, data) => {
+errorResponse = (res, statusCode, message, data) => {
   console.log(message, data); //Log error details...
 
   return res
