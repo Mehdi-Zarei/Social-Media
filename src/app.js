97d3389dcc1235d9b404ced40ce4dedea1c194cc
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-11-28 14:51:47 +0330

    coding error handler & set helmet package

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 
 //*Built-in middleware
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 //* Application-level middleware
 
 //* Routes
 
 //* Third-party middleware
+
+app.use(helmet());
+
+//* 404 Error Handler
+
+app.use((req, res) => {
+  console.log("This Path Not Found", req.path);
+  return res.status(404).json({ message: "Oops!! Page Not Found!!" });
+});
 
 module.exports = app;
