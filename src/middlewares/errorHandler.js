53b6fc6f1b9f6f59500e2c8dfcd3f49e403b0ea9
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-11-29 18:03:02 +0330

    set console.log to see errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 module.exports.errorHandler = (err, req, res, next) => {
   // If there is a validation error or other specific errors
   if (err.errors) {
+    console.log(err.errors);
     return res.status(400).json({
       status: "error",
       message: err.errors[0],
@@ -9,6 +10,7 @@ module.exports.errorHandler = (err, req, res, next) => {
 
   // If an unexpected error occurs
   if (err) {
+    console.log(err);
     return res.status(500).json({
       status: "error",
       message: "An unknown error occurred",
