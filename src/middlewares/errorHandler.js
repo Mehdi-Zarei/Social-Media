a400c0af245c6dc1a1aa485cecaf118e83266753
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-11-29 17:10:55 +0330

    coding error handler middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,3 +1,19 @@
-exports.errorHandler = (err, req, res, next) => {
-  //Todo : needed feature
+module.exports.errorHandler = (err, req, res, next) => {
+  // If there is a validation error or other specific errors
+  if (err.errors) {
+    return res.status(400).json({
+      status: "error",
+      message: err.errors[0],
+    });
+  }
+
+  // If an unexpected error occurs
+  if (err) {
+    return res.status(500).json({
+      status: "error",
+      message: "An unknown error occurred",
+    });
+  }
+
+  next();
 };
