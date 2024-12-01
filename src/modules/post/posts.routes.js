f9ca963fdf4d2c6a6495adad58e9ab65c9da03b6
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-01 16:03:19 +0330

    add create new post route

--- src/modules/post/posts.routes.js
+++ src/modules/post/posts.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("./posts.controller");
+const authMiddleware = require("../../middlewares/auth");
+
+router.route("/").post(authMiddleware, postController.createNewPost);
 
 module.exports = router;
