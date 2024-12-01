6e568c77293eb4af594a6f7b0131ad804ffd6ade
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-01 19:24:34 +0330

    coding post router

--- src/modules/post/posts.routes.js
+++ src/modules/post/posts.routes.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const { createUploader } = require("../../middlewares/uploader");
 const postController = require("./posts.controller");
 const authMiddleware = require("../../middlewares/auth");
 
-router.route("/").post(authMiddleware, postController.createNewPost);
+// Dynamic destination for posts (images and videos)
+const postDynamicDestination = (file) => {
+  if (/image/.test(file.mimetype)) return "images";
+  if (/video/.test(file.mimetype)) return "videos";
+  return "others";
+};
+
+// Configure the uploader for posts
+const postUploader = createUploader(
+  "./public/uploads/posts",
+  /jpeg|jpg|png|webp|mp4|avi|mkv/,
+  20 * 1024 * 1024, // 20 MB max file size
+  postDynamicDestination
+);
+
+// Route for uploading posts
+
+router
+  .route("/")
+  .post(
+    authMiddleware,
+    postUploader.single("media", 20),
+    postController.createNewPost
+  );
 
 module.exports = router;
