0dfb8f614ed33db7771cf9f4b119dcc5904b8b41
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-01 16:13:32 +0330

    create auth middleware to User identification

---
+++ src/middlewares/auth.js
@@ -0,0 +1,43 @@
+const userModel = require("../../models/user");
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.cookies["access-token"];
+    if (!token) {
+      return res
+        .status(401)
+        .json({
+          message:
+            "Pleas Register Or Login First, Then Try To Upload New Posts !!",
+        });
+    }
+
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    if (!payload) {
+      return res
+        .status(401)
+        .json({
+          message:
+            "Pleas Register Or Login First And Try To Upload New Posts !!",
+        });
+    }
+
+    const user = await userModel.findOne({ _id: payload._id }).lean();
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({
+          message:
+            "Pleas Register Or Login First And Try To Upload New Posts !!",
+        });
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
