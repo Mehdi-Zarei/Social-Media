0b64ffa122694c7aad417376cdd7cb87c031b142
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-02 18:40:02 +0330

    1-coding "follower & following Count" to show users in showUserPage func 2-coding show user follower func

--- src/modules/pages/page.controller.js
+++ src/modules/pages/page.controller.js
@@ -20,20 +20,31 @@ exports.showUserPage = async (req, res, next) => {
 
     const hasAccess = await hasAccessToPage(userID, pageID);
 
-    const followed = await followModel.findOne({
+    const IsItFollowed = await followModel.findOne({
       follower: userID._id,
       following: pageID,
     });
 
+    const followerCount = await followModel
+      .find({ following: pageID })
+      .countDocuments();
+    const followingCount = await followModel
+      .find({ follower: pageID })
+      .countDocuments();
+
     if (!hasAccess) {
       return res.status(403).json({
         message: "Follow Page To Show Content !!",
-        followed: Boolean(followed),
+        isThisPageFollowed: Boolean(IsItFollowed),
+        followerCount,
+        followingCount,
       });
     }
     return res.status(200).json({
       message: "User Can See Page Content.",
-      followed: Boolean(followed),
+      isThisPageFollowed: Boolean(IsItFollowed),
+      followerCount,
+      followingCount,
     });
   } catch (error) {
     next(error);
@@ -121,6 +132,52 @@ exports.unFollowUsers = async (req, res, next) => {
     return res
       .status(200)
       .json({ message: "You have successfully unfollowed this page." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.showUserFollowers = async (req, res, next) => {
+  try {
+    const userID = req.user;
+    const { pageID } = req.params;
+
+    if (!mongoose.isValidObjectId(pageID)) {
+      return res.status(400).json({ message: "Page ID Not Valid !!" });
+    }
+
+    const isPageExist = await userModel.findOne({ _id: pageID });
+
+    if (!isPageExist) {
+      return res.status(404).json({ message: "Page Not Found  !!" });
+    }
+
+    const IsItFollowed = await followModel.findOne({
+      follower: userID._id,
+      following: pageID,
+    });
+
+    if (!Boolean(IsItFollowed) && isPageExist.isPrivate === true) {
+      return res.status(401).json({
+        message:
+          "It is not possible to view the list of followers of personal pages before following them.",
+      });
+    }
+
+    const userFollowers = await followModel
+      .find({ following: pageID })
+      .select("follower")
+      .populate("follower", "name userName");
+
+    const formattedFollowers = userFollowers.map((follow) => follow.follower);
+
+    if (userFollowers.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "This user has no followers yet." });
+    }
+
+    return res.status(200).json({ userFollowers: formattedFollowers });
   } catch (error) {
     next(error);
   }
