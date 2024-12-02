167eb19b2fabd6ddc1ced7a2da023692a7cffe02
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-02 14:33:08 +0330

    coding showUserPage route

---
+++ src/modules/pages/page.routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+// const { createUploader } = require("./uploader");
+const pageController = require("./page.controller");
+const authMiddleware = require("../../middlewares/auth");
+
+//// Configure the uploader for profile pictures
+// const profileUploader = createUploader(
+//   "./public/uploads/profile",
+//   /jpeg|jpg|png|webp/,
+//   5 * 1024 * 1024 // 5 MB max file size
+// );
+
+// // Route for uploading profile picture
+// router.post(
+//   "/upload-profile",
+//   profileUploader.single("profile"),
+//   (req, res) => {
+//     // Access the uploaded file through req.file
+//     res.json({
+//       message: "Profile uploaded successfully!",
+//       file: req.file, // Details of the uploaded file
+//     });
+//   }
+// );
+
+router.route("/:pageID").get(authMiddleware, pageController.showUserPage);
+
+module.exports = router;
