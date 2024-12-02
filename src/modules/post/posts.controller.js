a521a32b11a17abaa016f167c183183f0a95d5bc
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-02 11:41:14 +0330

    Checking user account verification before creating a new post

--- src/modules/post/posts.controller.js
+++ src/modules/post/posts.controller.js
@@ -1,7 +1,21 @@
 const postsModel = require("../../../models/post");
+const userModel = require("../../../models/user");
 
 exports.createNewPost = async (req, res, next) => {
   try {
+    //Authentication from middleware auth
+    const user = req.user;
+
+    const isUserAccountVerified = await userModel
+      .findOne({
+        $and: [{ _id: user._id }, { isVerified: true }],
+      })
+      .lean();
+
+    if (!isUserAccountVerified) {
+      return res.status(401).json({ message: "Please verify your account." });
+    }
+
     const { description, hashtags } = req.body;
     const tags = hashtags.split(",");
 
@@ -10,10 +24,6 @@ exports.createNewPost = async (req, res, next) => {
     if (!req.file) {
       return res.status(400).json({ message: "No file uploaded!" });
     }
-
-    //Authentication from middleware auth
-
-    const user = req.user;
 
     // Check file type (photo or video)
 
