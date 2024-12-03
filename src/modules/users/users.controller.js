434ca2bea6ffc583dd4769addf1d9108f33525e9
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-03 13:54:43 +0330

    coding update user profile info func

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,9 +1,59 @@
-const userModel = require("../../../models/user");
-
 const fs = require("fs");
 const path = require("path");
+const userModel = require("../../../models/user");
 
-exports.updateUserProfile = async (req, res, next) => {
+exports.getUserProfileInfo = async (req, res, next) => {
+  try {
+    const user = req.user._id;
+    const userData = await userModel.findById(user);
+
+    return res.status(200).json({
+      name: userData.name,
+      userName: userData.userName,
+      email: userData.email,
+      biography: userData.biography,
+      profilePicture: userData.profilePicture,
+      isPrivate: userData.isPrivate,
+      isVerified: userData.isVerified,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateUserProfileInfo = async (req, res, next) => {
+  try {
+    const user = req.user._id;
+
+    const { name, userName, email, password, biography, isPrivate } = req.body;
+
+    const userData = await userModel.findById(user);
+
+    // Check if each field has changed and update
+    if (name) userData.name = name;
+
+    if (userName) userData.userName = userName;
+
+    if (email) userData.email = email;
+
+    if (biography) userData.biography = biography;
+
+    if (password) userData.password = password;
+
+    if (isPrivate !== undefined) userData.isPrivate = isPrivate;
+
+    //Save new user info
+    await userData.save();
+
+    return res.status(200).json({
+      message: "User profile updated successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateUserProfilePicture = async (req, res, next) => {
   try {
     const userID = req.user._id;
 
