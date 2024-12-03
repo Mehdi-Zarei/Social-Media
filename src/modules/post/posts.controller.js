de3bc05bd148a1c615cf3bd9f9c362877da6c861
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-03 16:09:54 +0330

    coding likeOrDislikePost func

--- src/modules/post/posts.controller.js
+++ src/modules/post/posts.controller.js
@@ -1,5 +1,8 @@
 const postsModel = require("../../../models/post");
 const userModel = require("../../../models/user");
+const likeModel = require("../../../models/like");
+const { default: mongoose } = require("mongoose");
+const hasAccessToUserPage = require("../../utils/hasAccessToUserPage");
 
 exports.createNewPost = async (req, res, next) => {
   try {
@@ -52,6 +55,50 @@ exports.createNewPost = async (req, res, next) => {
     });
 
     return res.status(201).json({ message: "New Post Created Successfully." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.likeOrDislikePost = async (req, res, next) => {
+  try {
+    const user = req.user._id;
+
+    const { postID } = req.params;
+
+    if (!mongoose.isValidObjectId(postID)) {
+      return res.status(400).json({ message: "Post ID Not Valid !!" });
+    }
+
+    const isPostExist = await postsModel.findOne({ _id: postID });
+
+    if (!isPostExist) {
+      return res.status(404).json({ message: "Post Not Found !!" });
+    }
+
+    const hasAccessToPost = await hasAccessToUserPage(
+      user,
+      isPostExist.user.toString()
+    );
+
+    if (!hasAccessToPost) {
+      return res
+        .status(401)
+        .json({ message: "You don't have access to private posts !!" });
+    }
+
+    const likeOrDislikePost = await likeModel.findOne({ post: postID, user });
+
+    if (likeOrDislikePost) {
+      await likeModel.findByIdAndDelete(likeOrDislikePost._id);
+      return res.status(200).json({ message: "Post disliked successfully." });
+    } else {
+      likeModel.create({
+        post: postID,
+        user,
+      });
+      return res.status(200).json({ message: "Post liked successfully." });
+    }
   } catch (error) {
     next(error);
   }
