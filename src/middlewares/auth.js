708f36f8b9ec88635eb2b54c0f696667ae2640fa
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-03 13:53:52 +0330

    fix message info

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,34 +5,25 @@ module.exports = async (req, res, next) => {
   try {
     const token = req.cookies["access-token"];
     if (!token) {
-      return res
-        .status(401)
-        .json({
-          message:
-            "Pleas Register Or Login First, Then Try To Upload New Posts !!",
-        });
+      return res.status(401).json({
+        message: "Pleas Register Or Login First.",
+      });
     }
 
     const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     if (!payload) {
-      return res
-        .status(401)
-        .json({
-          message:
-            "Pleas Register Or Login First And Try To Upload New Posts !!",
-        });
+      return res.status(401).json({
+        message: "Pleas Register Or Login First And Try To Upload New Posts !!",
+      });
     }
 
     const user = await userModel.findOne({ _id: payload._id }).lean();
 
     if (!user) {
-      return res
-        .status(401)
-        .json({
-          message:
-            "Pleas Register Or Login First And Try To Upload New Posts !!",
-        });
+      return res.status(401).json({
+        message: "Pleas Register Or Login First And Try To Upload New Posts !!",
+      });
     }
     req.user = user;
 
