3595254aa9a530e9d79358961c6166444a41208d
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-13 16:24:13 +0330

    coding refreshToken and forget password and reset password routes

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -4,5 +4,9 @@ const authController = require("./auth.controller");
 
 router.route("/register").post(authController.register);
 router.route("/login").post(authController.login);
+router.route("/refresh").post(authController.refreshToken);
+
+router.route("/forget-password").post(authController.forgetPassword);
+router.route("/reset-password/:token").post(authController.resetPassword);
 
 module.exports = router;
